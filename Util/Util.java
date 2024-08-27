db399811408bed53ddd9500c778a8b1c23138f72
Tina <liutianli2011@gmail.com>
2024-08-28 00:55:30 +0800

    updating Util class

---
+++ Util/Util.java
@@ -0,0 +1,10 @@
+package Util;
+
+public class Util {
+
+    public static void print2DArray(int[][]arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("[ " + arr[i][0] + "," + arr[i][1] + " ]");
+        }
+    }
+}
